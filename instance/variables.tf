416f8b334cbc0799c02c739dd4deac67e4dfd665
Yolanda Lopez <yolita2402@gmail.com>
2019-08-14 10:14:23 -0500

    Creacion de modules

---
+++ instance/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+    default=""
+    description="Ami Id"  
+}
+variable "instance_type" {
+  
+}
+variable "tags" {
+
+}
+variable "sg_name" {
+  
+}
+variable "ingress_rules" {
+  
+}
+
+
+
